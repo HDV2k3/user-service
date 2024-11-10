071ff9a47548fa0f069761c57e2468990f39efa6
Huỳnh Đắc Việt <dacviethuynh@gmail.com>
2024-11-10 22:13:37 +0700

    auth api

--- src/main/java/com/user/identity/configuration/OpenAPIConfig.java
+++ src/main/java/com/user/identity/configuration/OpenAPIConfig.java
@@ -1,5 +1,7 @@
 package com.user.identity.configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,12 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenAPIConfig {
 
     @Bean
